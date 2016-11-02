87c49f9a92fa0714199bb5f1e1541d6240a9ba7e
kovacskornel <ztoth@precognox.com>
2016-11-02 11:53:25 +0100

    TLOG

---
+++ src/tlog16java/TLOG16Java.java
@@ -0,0 +1,27 @@
+package tlog16java;
+import java.time.*;
+
+
+/*
+ * @author precognox kkovacs
+ */
+
+public class TLOG16Java {
+
+    long requiredMinPerDay = 450;
+    LocalDate actualDay = LocalDate.now();
+    String taskId,comment;
+    LocalTime startTime, endTime;
+    YearMonth date;
+    
+    long sumPerDay;
+    long requiredMinPerMonth;
+    long sumPerMontg;
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+
