99bb62dc81fe64aa964fb36acda71ff58a0c86fc
kovacskornel <ztoth@precognox.com>
2016-11-03 13:39:57 +0100

    Started making UI

--- src/tlog16java/TLOG16Java.java
+++ src/tlog16java/TLOG16Java.java
@@ -1,11 +1,11 @@
 package tlog16java;
-
 /*
  * @author precognox kkovacs
  */
 
 public class TLOG16Java {
+    static TimeLoggerUI useri = new TimeLoggerUI();
     public static void main(String[] args) {
-        
+        useri.menu();
     }
 }
